bd892c1ac1508a9eeea7944d9e714eaae8d15d93
Marti977 <Martin.Devanathan@gmail.com>
2022-05-25 11:13:04 +0300

    test

--- Isola/Isola/Game1.cs
+++ Isola/Isola/Game1.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
 
 namespace Isola
 {
@@ -8,6 +13,7 @@ namespace Isola
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _tex;
 
         public Game1()
         {
@@ -26,7 +32,8 @@ namespace Isola
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            _tex = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+            base.LoadContent();
             // TODO: use this.Content to load your game content here
         }
 
@@ -42,11 +49,73 @@ namespace Isola
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
+
+        public void PlayerAndMovement( Square[,] field, int startingX, int startingY)
+        {
+           Rectangle player = new Rectangle();
+           
+            Point mousePos = Mouse.GetState().Position;
+            Rectangle mouseRect = new Rectangle(mousePos.X, mousePos.Y, 2, 2);
+            List<Square> avalibleSquares = new List<Square>();
+            Square currentSquare = new Square((avalibleSquares.First(x => x.rcc.Intersects(player)).rcc), false);
+            int currentSquarePosX = 0;
+            int currentSquarePosY = 0;
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                int y = 0;
+                if (i == field.GetLength(1))
+                {
+                    i = 0;
+                    y++;
+                }
+                if (field[i, y] == currentSquare)
+                {
+                    currentSquarePosX = i;
+                    currentSquarePosY = y;
+                }
+            }
+
+            foreach (var square in field)
+            {
+                if (square.IsDestroyed == false && (!field[currentSquarePosX + 1, currentSquarePosY].IsDestroyed || !field[currentSquarePosX - 1, currentSquarePosY].IsDestroyed  || !field[currentSquarePosX, currentSquarePosY + 1].IsDestroyed || !field[currentSquarePosX , currentSquarePosY - 1].IsDestroyed || !field[currentSquarePosX + 1, currentSquarePosY + 1].IsDestroyed || !field[currentSquarePosX + 1, currentSquarePosY - 1].IsDestroyed || !field[currentSquarePosX - 1, currentSquarePosY + 1].IsDestroyed || !field[currentSquarePosX - 1, currentSquarePosY - 1].IsDestroyed))
+                {
+                    avalibleSquares.Add(square);
+                }    
+            }
+            foreach (var CurrentSquare in avalibleSquares)
+            {
+                if (mouseRect.Intersects(CurrentSquare.rcc))
+                {
+                    player.X = CurrentSquare.rcc.X;
+                    player.Y = CurrentSquare.rcc.Y;
+                }
+            }
+        }
+
+        public void Destroy(Square CurrentSquare)
+        {
+            Point mousePos = Mouse.GetState().Position;
+            Rectangle mouseRect = new Rectangle(mousePos.X, mousePos.Y, 2, 2);
+
+        }
     }
+   public class Square
+    {
+        public Rectangle rcc = new Rectangle();
+        public bool IsDestroyed;
+        public Square(Rectangle Rcc, bool ISDestroyed) 
+        {
+            rcc = Rcc;
+            IsDestroyed = ISDestroyed;
+        }
+    }
+
+   
 }
