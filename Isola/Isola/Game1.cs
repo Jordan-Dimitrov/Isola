4a22da57ed70ad603a894f8ab1586371e9de1f0a
OKAY808 <georgedautov808@gmail.com>
2022-05-25 10:59:56 +0300

    added field

--- Isola/Isola/Game1.cs
+++ Isola/Isola/Game1.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Linq;
+using System;
 
 namespace Isola
 {
+    class Square 
+    {
+        public Square() 
+        {
+            _Transform = new Rectangle(0, 0, SquareSize, SquareSize);
+            IsDestroyed = false;
+        }
+
+        public Square(int x, int y) 
+        {
+            _Transform = new Rectangle(x, y, SquareSize, SquareSize);
+            IsDestroyed = false;
+        }
+
+        public Rectangle GetTransform() => _Transform;
+
+        public static int SquareSize = 60;
+        private Rectangle _Transform;
+        public bool IsDestroyed;
+    }
+
     public class Game1 : Game
     {
+        private Square[,] _Field;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _WhiteTex;
+
 
         public Game1()
         {
@@ -18,8 +44,21 @@ namespace Isola
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _Field = new Square[6, 8];
+            int xOffset = (Square.SquareSize + 2) * _Field.GetLength(1), yOffset = (Square.SquareSize + 2) * _Field.GetLength(0);
+            
+            xOffset = Window.ClientBounds.Width / 2 - xOffset / 2;
+            yOffset = Window.ClientBounds.Height / 2 - yOffset / 2;
 
+            for (int y = 0; y < _Field.GetLength(0); y++)
+                for (int x = 0; x < _Field.GetLength(1); x++)
+                    _Field[y, x] = new Square((x * (Square.SquareSize + 2)) + xOffset, (y * (Square.SquareSize + 2)) + yOffset);
+
+            _WhiteTex = new Texture2D(GraphicsDevice, 1, 1);
+            uint[] whiteTexturedata = new uint[] { 0xffffffff };
+            _WhiteTex.SetData(whiteTexturedata);
+
+            // TODO: Add your initialization logic 
             base.Initialize();
         }
 
@@ -35,6 +74,7 @@ namespace Isola
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -42,9 +82,15 @@ namespace Isola
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            for (int y = 0; y < _Field.GetLength(0); y++)
+                for (int x = 0; x < _Field.GetLength(1); x++)
+                    _spriteBatch.Draw(_WhiteTex, _Field[y, x].GetTransform(), !_Field[y, x].IsDestroyed ? Color.White : Color.Black);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
