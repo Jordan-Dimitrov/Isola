26a28641de9e745f3f80d7b8dc55675b454461a0
Jordan-Dimitrov <dani27game@gmail.com>
2022-05-25 12:14:44 +0300

    changed ui

--- Isola/Isola/Game1.cs
+++ Isola/Isola/Game1.cs
@@ -6,15 +6,15 @@ using System;
 
 namespace Isola
 {
-    public class Square 
+    public class Square
     {
-        public Square() 
+        public Square()
         {
             _Transform = new Rectangle(0, 0, SquareSize, SquareSize);
             IsDestroyed = false;
         }
 
-        public Square(int x, int y) 
+        public Square(int x, int y)
         {
             _Transform = new Rectangle(x, y, SquareSize, SquareSize);
             IsDestroyed = false;
@@ -34,14 +34,23 @@ namespace Isola
         private SpriteBatch _spriteBatch;
         private Texture2D _WhiteTex;
 
-        private bool hasGameBegun = false;
+        private bool hasGameBegun = true;
         private bool player1Wins;
         private bool player2Wins;
         private SpriteFont _font;
-        private Rectangle _BeginTextTransform;
+        private Rectangle _BeginTextTransform1, _BeginTextTransform2;
+        private enum PlayMode 
+        {
+            None = 0,
+            SinglePlayer,
+            MultiPlayer
+        }
+
+        PlayMode _playMode;
+
         public Game1()
         {
-            
+
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
@@ -51,7 +60,7 @@ namespace Isola
         {
             _Field = new Square[6, 8];
             int xOffset = (Square.SquareSize + 2) * _Field.GetLength(1), yOffset = (Square.SquareSize + 2) * _Field.GetLength(0);
-            
+
             xOffset = Window.ClientBounds.Width / 2 - xOffset / 2;
             yOffset = Window.ClientBounds.Height / 2 - yOffset / 2;
 
@@ -75,62 +84,86 @@ namespace Isola
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _font = Content.Load<SpriteFont>("Arial");
 
-            Vector2 textSize = _font.MeasureString("Press space to continue");
-            Vector2 textPos = new Vector2(Window.ClientBounds.Width / 2 - 100, Window.ClientBounds.Height / 2 - 50);
-            _BeginTextTransform = new Rectangle(textPos.ToPoint(), textSize.ToPoint());
-            // TODO: use this.Content to load your game content here
+            Vector2 textSize = _font.MeasureString("Singleplayer");
+            Vector2 textPos = new Vector2(Window.ClientBounds.Width / 2 - 370, 100);
+            _BeginTextTransform1 = new Rectangle(textPos.ToPoint(), textSize.ToPoint());
+
+            Vector2 textSize2 = _font.MeasureString("Multiplayer");
+            Vector2 textPos2 = new Vector2(Window.ClientBounds.Width / 2 - 370, Window.ClientBounds.Height / 2);
+            _BeginTextTransform2 = new Rectangle(textPos2.ToPoint(), textSize2.ToPoint());
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Space))
-            {
-                hasGameBegun = true;
-            }
             if (Keyboard.GetState().IsKeyDown(Keys.R))
             {
-                hasGameBegun = false;
+                hasGameBegun = true;
+
+            }
+            Point mousePos = Mouse.GetState().Position;
+            Rectangle mouseRect = new Rectangle(mousePos, new Point(2, 2));
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                if (mouseRect.Intersects(_BeginTextTransform1))
+                {
+                    hasGameBegun = false;
+                    _playMode = PlayMode.SinglePlayer;
+
+                }
+                else if (mouseRect.Intersects(_BeginTextTransform2))
+                {
+                    hasGameBegun = false;
+                    _playMode = PlayMode.MultiPlayer;
+                }
             }
 
             base.Update(gameTime);
         }
 
-        private void DrawField() 
+        private void DrawField()
         {
             for (int y = 0; y < _Field.GetLength(0); y++)
                 for (int x = 0; x < _Field.GetLength(1); x++)
                     _spriteBatch.Draw(_WhiteTex, _Field[y, x].GetTransform(), !_Field[y, x].IsDestroyed ? Color.White : Color.Black);
         }
-
-        private void DrawMainMenu() 
-        {
-            _spriteBatch.DrawString(_font, $"Press space to start playing", _BeginTextTransform.Location.ToVector2(), Color.White);
-        }
-
-        private void DrawEndScreen() 
-        {
-            Vector2 textSize = _font.MeasureString($"Player {(player1Wins ? 1 : 2)} has won");
-            Vector2 textPos = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
-            _spriteBatch.DrawString(_font, $"Player {(player1Wins ? 1 : 2)} has won! Press R to restart", textPos, Color.White);
-        }
-
         protected override void Draw(GameTime gameTime)
         {
+            player2Wins = true;
             GraphicsDevice.Clear(Color.Black);
-
             _spriteBatch.Begin();
-
-            if (!hasGameBegun)
+            if (hasGameBegun == true)
             {
-                DrawMainMenu();
+                StartScreen();
             }
             else
             {
-                DrawField();
-                DrawEndScreen();
+                //DrawField();
+                EndScreen();
             }
             _spriteBatch.End();
             base.Draw(gameTime);
         }
+        public void StartScreen()
+        {
+
+            _spriteBatch.DrawString(_font, $"Singleplayer", _BeginTextTransform1.Location.ToVector2(), Color.White);
+
+
+            _spriteBatch.DrawString(_font, $"Multiplayer", _BeginTextTransform2.Location.ToVector2(), Color.White);
+        }
+        public void EndScreen()
+        {
+            if (player1Wins || player2Wins) 
+            {
+                string player = player1Wins ? "Player 1" : "Player 2";
+                if (player2Wins && _playMode == PlayMode.SinglePlayer)
+                    player = "Computer";
+
+                Vector2 textSize = _font.MeasureString($"{player} has won");
+                Vector2 textPos = new Vector2(Window.ClientBounds.Width / 2 - textSize.X / 2, Window.ClientBounds.Height / 2 - textSize.Y / 2);
+                _spriteBatch.DrawString(_font, $"{player} has won! Press R to restart", textPos, Color.White);
+            }
+        }
     }
+
 }
